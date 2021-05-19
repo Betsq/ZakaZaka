1d2905a4eb2d2651704a6d7d04f592f000d85a8a
Betsq9 <betsq9@gmail.com>
2021-05-19 23:36:48 +0300

    Add route

--- ZakaZaka/ClientApp/src/app/app.module.ts
+++ ZakaZaka/ClientApp/src/app/app.module.ts
@@ -1,11 +1,18 @@
 ﻿import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
+import { Routes, RouterModule} from "@angular/router";
+
 import { AppComponent } from "./app.component";
+import {Home} from "./home/home.component";
+
+const appRoutes: Routes = [
+  {path: "", component: Home},
+];
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent],
+  declarations: [AppComponent, Home],
   bootstrap: [ AppComponent]
 })
 export class AppModule{
