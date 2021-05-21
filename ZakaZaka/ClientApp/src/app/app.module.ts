308110bc90f2f5f2c61b7ab39f5bd511a625fb06
Betsq9 <betsq9@gmail.com>
2021-05-21 14:42:26 +0300

    Add main header

--- ZakaZaka/ClientApp/src/app/app.module.ts
+++ ZakaZaka/ClientApp/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { Routes, RouterModule} from "@angular/router";
 import { AppComponent } from "./app.component";
 import {Home} from "./home/home.component";
 import {Slider} from "./home/addition/slider";
+import {Header} from "./header/header.component";
 
 const appRoutes: Routes = [
   { path: '', component: Home },
@@ -14,7 +15,7 @@ const appRoutes: Routes = [
 
 @NgModule({
   imports: [BrowserModule, FormsModule, RouterModule.forRoot(appRoutes)],
-  declarations: [AppComponent, Home, Slider],
+  declarations: [AppComponent, Home, Slider, Header],
   bootstrap: [ AppComponent]
 })
 export class AppModule{
