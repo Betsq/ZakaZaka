80059a82dcfcbf00285ca2b52b96190454447bca
Betsq9 <betsq9@gmail.com>
2021-05-24 12:14:27 +0300

    Add section reasons

--- ZakaZaka/ClientApp/src/app/home/home.component.ts
+++ ZakaZaka/ClientApp/src/app/home/home.component.ts
@@ -3,7 +3,7 @@
 @Component({
   selector: "home",
   templateUrl: "home.component.html",
-  styleUrls: ["/styles/home.category.component.css", "/styles/home.instruction.css"]
+  styleUrls: ["/styles/home.category.component.css", "/styles/home.instruction.css", "/styles/home.reasons.css"]
 })
 
 export class Home {
