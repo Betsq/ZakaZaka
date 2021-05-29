418ca13cb1c1badb41732afa8754bff0a382b5e8
Betsq9 <betsq9@gmail.com>
2021-05-29 23:39:50 +0300

    Add restaurant data service

---
+++ ZakaZaka/ClientApp/src/app/service/restuarantData.service.ts
@@ -0,0 +1,30 @@
+﻿import {Injectable} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+import  { Restaurant} from "../Model/restaurant";
+
+@Injectable()
+export class RestaurantDataService{
+  private url = "/api/Restaurant";
+
+  constructor(private http: HttpClient) {}
+
+  getRestaurants(){
+    return this.http.get(this.url);
+  }
+
+  getRestaurant(id: number){
+    return this.http.get(this.url + "/" + id)
+  }
+
+  createRestaurant(restaurant: Restaurant){
+    return this.http.post(this.url, restaurant)
+  }
+
+  updateRestaurant(restaurant: Restaurant){
+    return this.http.put(this.url, restaurant)
+  }
+
+  removeRestaurant(id: number){
+    return this.http.delete(this.url + "/" + id)
+  }
+}
