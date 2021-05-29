1702e7b99c94c69881ceaf865c1dc98b09d30763
Betsq9 <betsq9@gmail.com>
2021-05-29 23:40:31 +0300

    Add restaurant model

---
+++ ZakaZaka/ClientApp/src/app/Model/restaurant.ts
@@ -0,0 +1,10 @@
+﻿export class Restaurant{
+  constructor(
+    public id?: number,
+    public name?: string,
+    public minimumOrder?: number,
+    public costDelivery?: number,
+    public timeToDelivery?: Date,
+    public payToCard?: boolean
+  ) {}
+}
