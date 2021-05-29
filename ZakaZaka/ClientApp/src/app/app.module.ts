92ba079a2b8b20e6dfd28e27f8382a65658f7e62
Betsq9 <betsq9@gmail.com>
2021-05-29 23:38:38 +0300

    Add route to the manage restaurant page

--- ZakaZaka/ClientApp/src/app/app.module.ts
+++ ZakaZaka/ClientApp/src/app/app.module.ts
@@ -1,22 +1,26 @@
 ﻿import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
-import { Routes, RouterModule} from "@angular/router";
+import { Routes, RouterModule } from "@angular/router";
+import { HttpClientModule}  from "@angular/common/http";
 
 import { AppComponent } from "./app.component";
 import {Home} from "./home/home.component";
 import {Slider} from "./home/addition/slider";
 import {Header} from "./header/header.component";
 import {Footer} from "./footer/footer.component";
+import { RestaurantManageComponent } from "./restaurant-manage/restaurantManage.component"
 
 const appRoutes: Routes = [
   { path: '', component: Home },
+  { path: "restaurantManage", component: RestaurantManageComponent},
   { path: '**', redirectTo: '/' }
+
 ];
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, RouterModule.forRoot(appRoutes)],
-    declarations: [AppComponent, Home, Slider, Header, Footer],
+  imports: [BrowserModule, FormsModule, HttpClientModule, RouterModule.forRoot(appRoutes)],
+  declarations: [AppComponent, Home, Slider, Header, Footer, RestaurantManageComponent],
   bootstrap: [ AppComponent]
 })
 export class AppModule{
