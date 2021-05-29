8abc1b534326959bb870b811590ff596b49ca53f
Betsq9 <betsq9@gmail.com>
2021-05-29 17:43:40 +0300

    Add restaurant model

---
+++ ZakaZaka/Models/Restaurant.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ZakaZaka.Models
+{
+    public class Restaurant
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public int MinimumOrder { get; set; }
+        public int CostDelivery { get; set; }
+        public DateTime TimeToDelivery { get; set; }
+        public bool PayToCard { get; set; }
+    }
+}
