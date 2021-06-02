23ca37283854b9e58d0e0c53c2893debb41620c6
Betsq9 <betsq9@gmail.com>
2021-06-02 20:10:37 +0300

    Add fromater data binder

--- ZakaZaka/Service/FormDataBinder/FormDataJsonBinder.cs
+++ ZakaZaka/Service/FormDataBinder/FormDataJsonBinder.cs
@@ -1,7 +1,38 @@
-﻿namespace ZakaZaka.Service.FormDataBinder
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Newtonsoft.Json;
+
+namespace ZakaZaka.Service.FormDataBinder
 {
-    public class FormDataJsonBinder
+    public class FormDataJsonBinder : IModelBinder
     {
-        
+        public Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+            if(bindingContext == null) throw new ArgumentNullException(nameof(bindingContext));
+
+            // Fetch the value of the argument by name and set it to the model state
+            string fieldName = bindingContext.FieldName;
+            var valueProviderResult = bindingContext.ValueProvider.GetValue(fieldName);
+            if(valueProviderResult == ValueProviderResult.None) return Task.CompletedTask;
+            else bindingContext.ModelState.SetModelValue(fieldName, valueProviderResult);
+
+            // Do nothing if the value is null or empty
+            string value = valueProviderResult.FirstValue;
+            if(string.IsNullOrEmpty(value)) return Task.CompletedTask;
+
+            try
+            {
+                // Deserialize the provided value and set the binding result
+                object result = JsonConvert.DeserializeObject(value, bindingContext.ModelType);
+                bindingContext.Result = ModelBindingResult.Success(result);
+            }
+            catch(JsonException)
+            {
+                bindingContext.Result = ModelBindingResult.Failed();
+            }
+
+            return Task.CompletedTask;
+        }
     }
 }
