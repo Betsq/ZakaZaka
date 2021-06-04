d16f49bcf2d0aa27030d9fb69c93048e6bca06ae
Betsq9 <betsq9@gmail.com>
2021-06-05 00:20:39 +0300

    Add class for remove file

---
+++ ZakaZaka/Service/RemovingFile/RemoveFileFromServer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+
+namespace ZakaZaka.Service.RemovingFile
+{
+    public class RemoveFileFromServer : RemoveFile
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        
+        public RemoveFileFromServer(string path, IWebHostEnvironment webHostEnvironment)
+            : base(path)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        public override void Remove()
+        {
+            if (!File.Exists(_path))
+                throw new Exception($"The file in the path {_webHostEnvironment + _path} does not exist");
+            
+            File.Delete(_webHostEnvironment + _path);
+        }
+    }
+}
