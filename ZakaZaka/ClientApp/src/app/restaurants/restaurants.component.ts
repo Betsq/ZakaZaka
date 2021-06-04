92b581d40c0cef9246a80c166a81396225a58eb2
Betsq9 <betsq9@gmail.com>
2021-06-04 23:13:07 +0300

    Add restaurant output

--- ZakaZaka/ClientApp/src/app/restaurants/restaurants.component.ts
+++ ZakaZaka/ClientApp/src/app/restaurants/restaurants.component.ts
@@ -1,12 +1,23 @@
 ﻿import {Component, OnInit} from "@angular/core";
+import { RestaurantDataService} from "../service/restuarantData.service";
+import {Restaurant} from "../Model/restaurant";
 
 @Component({
   selector: "restaurants",
   templateUrl: "restaurants.component.html",
-  styleUrls: ["restaurants.component.less"]
+  styleUrls: ["restaurants.component.less"],
+  providers: [RestaurantDataService]
 })
 
 export class RestaurantsComponent implements OnInit{
+  restaurants: Restaurant[];
+  constructor(private dataService: RestaurantDataService) {}
+
   ngOnInit() {
+    this.loadRestaurants()
+  }
+
+  loadRestaurants(){
+    this.dataService.getRestaurants().subscribe((data: Restaurant[]) => this.restaurants = data);
   }
 }
