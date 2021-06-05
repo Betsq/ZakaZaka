f8d19d97e5b33ddb13d184dfa60c3622a6cabfc6
Betsq9 <betsq9@gmail.com>
2021-06-05 18:30:05 +0300

    Add data service for cuisine

---
+++ ZakaZaka/ClientApp/src/app/service/Data/cuisineData.service.ts
@@ -0,0 +1,10 @@
+﻿import {Injectable} from "@angular/core";
+import {DataService} from "./Data.service";
+import {HttpClient} from "@angular/common/http";
+
+@Injectable()
+export class CuisineDataService extends DataService{
+  constructor(protected http: HttpClient) {
+    super(http);
+  }
+}
