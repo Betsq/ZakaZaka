1452c5af194ae73ed850bd5d0ee1079e79f5411d
Betsq9 <betsq9@gmail.com>
2021-06-07 23:52:07 +0300

    Add remove and adding cusinse for a restaurant
    -Add send to server cusine for a restaurant

--- ZakaZaka/ClientApp/src/app/restaurant-manage/restaurantManage.component.ts
+++ ZakaZaka/ClientApp/src/app/restaurant-manage/restaurantManage.component.ts
@@ -3,6 +3,7 @@ import {RestaurantDataService} from "../service/Data/restuarantData.service";
 import { Restaurant } from "../Model/restaurant";
 import {RestaurantManageViewModel} from "../../ViewModel/RestaurantManageViewModel";
 import {Cuisine} from "../Model/cuisine";
+import {RestaurantCuisines} from "../Model/restaurantCuisine";
 
 @Component({
   selector: "restaurant-manage",
@@ -15,9 +16,13 @@ export class RestaurantManageComponent implements OnInit{
 
   restaurant: Restaurant = new Restaurant();
   restaurants: Restaurant[];
-  cuisines: Cuisine[];
+  cuisines: Cuisine[] = [];
 
+  cuisinesPost: Cuisine[] = [];
   file: FileList = null;
+
+
+  restaurantCuisine: RestaurantCuisines[] = [];
 
   isShowProducts: boolean = true;
   isShowCreate: boolean = false;
@@ -40,25 +45,57 @@ export class RestaurantManageComponent implements OnInit{
     this.cuisines = model.cuisines;
   }
 
+  addCuisine(item: boolean, cuisine: Cuisine){
+    console.log(this.cuisinesPost);
+    if(item === true){
+      this.cuisinesPost.push(cuisine);
+    }
+    else{
+      const index = this.getIndex(cuisine, this.cuisinesPost);
+
+      if(index > -1)
+        this.cuisinesPost.splice(index, 1);
+    }
+  }
+
+  private getIndex(cuisines: Cuisine, cuisinesPost: Cuisine[]): number{
+    for(let i = 0; i < cuisinesPost.length; i++){
+      if(cuisines.id == cuisinesPost[i].id){
+        return i;
+      }
+    }
+    return -1;
+  }
+
   save(){
     const formDate = this.formGenerate();
 
-    if(this.restaurant.id == null)
-      this.dataService.Post(formDate).subscribe((data: Restaurant) => this.loadProducts())
-    else
-      this.dataService.Put(formDate).subscribe(data => this.loadProducts())
+    if(this.restaurant.id == null){
+      this.dataService.Post(formDate).subscribe((data: Restaurant) => this.loadProducts());
+    }
+    else{
+      this.dataService.Put(formDate).subscribe(() => this.loadProducts())
+    }
 
     this.cancel();
   }
 
   update(restaurant: Restaurant){
     this.restaurant = restaurant;
+    this.restaurantCuisine = restaurant.restaurantCuisines;
+
+    for(let i = 0; i < this.restaurantCuisine.length; i++){
+      this.cuisinesPost.push(this.restaurant.restaurantCuisines[i].cuisine);
+    }
+
     this.show(false, false, true)
   }
 
   cancel(){
     this.file = null;
+    this.restaurantCuisine = [];
     this.restaurant = new Restaurant();
+    this.cuisinesPost = [];
     this.show(true, false, false);
   }
 
@@ -66,7 +103,7 @@ export class RestaurantManageComponent implements OnInit{
     let confirmToDelete: boolean = confirm("Are you sure you want to delete this item?");
 
     if(confirmToDelete)
-      this.dataService.Delete(restaurant.id).subscribe(data => this.loadProducts());
+      this.dataService.Delete(restaurant.id).subscribe(() => this.loadProducts());
   }
 
   add(){
@@ -97,11 +134,27 @@ export class RestaurantManageComponent implements OnInit{
   formGenerate(){
     const formDate = new FormData();
 
+    this.restaurant.restaurantCuisines = this.restaurantCuisine;
+
     formDate.append("restaurant", JSON.stringify(this.restaurant));
 
     if(this.file !== null)
       formDate.append("file", this.file[0], this.file[0].name);
 
+    if(this.cuisinesPost.length > 0)
+      formDate.append("cuisines", JSON.stringify(this.cuisinesPost));
+
     return formDate;
   }
+
+  hasCuisine(cuisineId: number) : boolean{
+    for(let i = 0; i < this.restaurant.restaurantCuisines.length; i++){
+      if(cuisineId == this.restaurant.restaurantCuisines[i].cuisine.id){
+        return true;
+      }
+    }
+    return false;
+  }
+
+
 }
