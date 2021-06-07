90f749833de0f1bc8e57e0ecd6cea24ff3ecf59a
Betsq9 <betsq9@gmail.com>
2021-06-07 23:55:27 +0300

    Add the service for adding and removing cuisine for a restaurant

---
+++ ZakaZaka/Service/RestaurantCuisines/RestaurantCuisineService.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using Microsoft.EntityFrameworkCore;
+using ZakaZaka.Context;
+using ZakaZaka.Models;
+using ZakaZaka.Models.Restaurants;
+
+namespace ZakaZaka.Service.RestaurantCuisines
+{
+    public class RestaurantCuisineService : IAdd, IRemove
+    {
+        private Restaurant _restaurant;
+        private List<Cuisine> _cuisines;
+        public RestaurantCuisineService(Restaurant restaurant, List<Cuisine> cuisines)
+        {
+            _restaurant = restaurant;
+            _cuisines = cuisines;
+        }
+        
+        public List<RestaurantCuisine> Add()
+        {
+            Validate();
+            
+            var listOfRestaurantCuisine = new List<RestaurantCuisine>();
+            var restaurantCuisine = new RestaurantCuisine() { };
+            
+            foreach (var cuisine in _cuisines)
+            {
+                if (!Exist(cuisine.Id))
+                {
+                    restaurantCuisine.CuisineId = cuisine.Id;
+                    restaurantCuisine.RestaurantId = _restaurant.Id;
+                    
+                    listOfRestaurantCuisine.Add(restaurantCuisine);
+                }
+            }
+
+            return listOfRestaurantCuisine;
+        }
+
+        public List<RestaurantCuisine> Remove(List<RestaurantCuisine> restaurantCuisinesList)
+        {
+            Validate();
+
+            var listOfRestaurantCuisine = restaurantCuisinesList;
+            
+            foreach (var cuisine in _cuisines)
+            {
+                var s = restaurantCuisinesList.FirstOrDefault(i => i.CuisineId == cuisine.Id);
+                
+                if (s != null)
+                {
+                    listOfRestaurantCuisine.Remove(s);
+                }
+            }
+
+            return listOfRestaurantCuisine;
+        }
+
+        private void Validate()
+        {
+            if (_restaurant == null) throw new NullReferenceException("Restaurant is null");
+
+            if (_cuisines == null) throw new NullReferenceException("Cuisines is null");
+        }
+
+        private bool Exist(int cuisineId)
+        {
+            var hasCuisine = _restaurant.RestaurantCuisines.FirstOrDefault(item => item.CuisineId == cuisineId);
+
+            return hasCuisine != null;
+        }
+    }
+}
