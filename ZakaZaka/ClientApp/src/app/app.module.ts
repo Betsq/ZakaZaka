a0ee331c306133db90801ac4700f36756484cd40
Betsq9 <betsq9@gmail.com>
2021-06-12 22:26:13 +0300

    Add routes for the restaurant component

--- ZakaZaka/ClientApp/src/app/app.module.ts
+++ ZakaZaka/ClientApp/src/app/app.module.ts
@@ -14,24 +14,35 @@ import {CuisineManageComponent} from "./cusinine-manage/cuisineManage.component"
 import {RestaurantsComponent} from "./restaurants/restaurants.component";
 import {RestaurantFoodManageComponent} from "./restaurantFood-manage/restaurantFoodManage.component";
 
+import {RestaurantComponent} from "./restaurant/restaurant.component";
+import {RestaurantMenuComponent} from "./restaurant/child.component/menu/restaurantMenu.component"
+import {RestaurantReviewComponent} from "./restaurant/child.component/review/restaurantReview.component";
+
+//restaurant's the child components
+const restaurantRoutes: Routes = [
+  {path: "menu", component: RestaurantMenuComponent},
+  {path: "reviews", component: RestaurantReviewComponent}
+];
+
 const appRoutes: Routes = [
   { path: "", component: Home },
   { path: "restaurantManage", component: RestaurantManageComponent},
   { path: "cuisineManage", component: CuisineManageComponent},
   { path: "restaurants", component: RestaurantsComponent},
   { path: "restaurantFoodManage", component: RestaurantFoodManageComponent},
+  { path: "restaurant/:id", component: RestaurantComponent},
+  { path: "restaurant/:id", component: RestaurantComponent, children: restaurantRoutes},
   { path: "**", redirectTo: "/" }
+]
 
-];
 
 @NgModule({
   imports: [BrowserModule, FormsModule,ReactiveFormsModule, HttpClientModule, RouterModule.forRoot(appRoutes)],
   declarations: [
     AppComponent, Home, Slider, Header, Footer, RestaurantManageComponent, RestaurantsComponent,
-    CuisineManageComponent, RestaurantFoodManageComponent
+    CuisineManageComponent, RestaurantFoodManageComponent, RestaurantComponent, RestaurantMenuComponent,
+    RestaurantReviewComponent
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule{
-
-}
+export class AppModule{}
