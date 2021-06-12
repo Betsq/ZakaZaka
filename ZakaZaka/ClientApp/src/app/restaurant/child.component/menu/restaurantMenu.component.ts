524164fe622de04d86b6d9bcb42ef477827a0d2d
Betsq9 <betsq9@gmail.com>
2021-06-12 22:25:22 +0300

    Add child cpmponents for the restaurant component

---
+++ ZakaZaka/ClientApp/src/app/restaurant/child.component/menu/restaurantMenu.component.ts
@@ -0,0 +1,11 @@
+﻿import {Component} from "@angular/core";
+
+@Component({
+  selector: "restaurant-menu",
+  templateUrl: "restaurantMenu.component.html",
+  styleUrls: ["restaurantMenu.component.less"]
+})
+
+export class RestaurantMenuComponent{
+
+}
