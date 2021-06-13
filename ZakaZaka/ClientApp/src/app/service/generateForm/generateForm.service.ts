9c8a6ce91550ea7fb6d13c3c39a72ea1dc8256c8
Betsq9 <betsq9@gmail.com>
2021-06-13 19:33:42 +0300

    Add service for the generate form

---
+++ ZakaZaka/ClientApp/src/app/service/generateForm/generateForm.service.ts
@@ -0,0 +1,41 @@
+﻿import {Injectable} from "@angular/core";
+
+@Injectable()
+export class GenerateFormService{
+
+
+  constructor() {}
+
+  generate(data: {name: string, param: any}[], file?: FileList) : FormData{
+    data = data.concat(data);
+
+    const formDate = new FormData();
+    for(let i = 0; i < data.length; i++){
+      formDate.append(data[i].name, JSON.stringify(data[i].param));
+    }
+
+    if(file != null){
+      formDate.append("file", file[0], file[0].name);
+    }
+
+    return formDate;
+  }
+}
+  /*
+  generate(data: [{name: string, param: any}], file?: FileList) : FormData{
+    const formDate = new FormData();
+    for(let i = 0; i < data.length; i++){
+      formDate.append(data[i].name, JSON.stringify(this.))
+    }
+    if(file != null){
+      formDate.append("file", file[0], file[0].name);
+    }
+
+    formDate.append("restaurantFood", JSON.stringify(this.restaurantFood));
+    formDate.append("restaurantId", JSON.stringify(this.restaurantId));
+
+    return formDate;
+}*/
+
+
+
