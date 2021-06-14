94c2e25a3a079022a783739f217f4bb43576d25f
Betsq9 <betsq9@gmail.com>
2021-06-14 22:47:09 +0300

    Change component paths

--- ZakaZaka/ClientApp/src/app/app.module.ts
+++ ZakaZaka/ClientApp/src/app/app.module.ts
@@ -5,18 +5,18 @@ import { Routes, RouterModule } from "@angular/router";
 import { HttpClientModule}  from "@angular/common/http";
 
 import { AppComponent } from "./app.component";
-import {Home} from "./home/home.component";
-import {Slider} from "./home/addition/slider";
-import {Header} from "./header/header.component";
-import {Footer} from "./footer/footer.component";
-import {RestaurantManageComponent} from "./restaurant-manage/restaurantManage.component"
-import {CuisineManageComponent} from "./cusinine-manage/cuisineManage.component";
-import {RestaurantsComponent} from "./restaurants/restaurants.component";
-import {RestaurantFoodManageComponent} from "./restaurantFood-manage/restaurantFoodManage.component";
+import {Home} from "./Components/home/home.component";
+import {Slider} from "./Components/home/addition/slider";
+import {Header} from "./Components/header/header.component";
+import {Footer} from "./Components/footer/footer.component";
+import {RestaurantManageComponent} from "./Components/restaurant-manage/restaurantManage.component"
+import {CuisineManageComponent} from "./Components/cusinine-manage/cuisineManage.component";
+import {RestaurantsComponent} from "./Components/restaurants/restaurants.component";
+import {RestaurantFoodManageComponent} from "./Components/restaurantFood-manage/restaurantFoodManage.component";
 
-import {RestaurantComponent} from "./restaurant/restaurant.component";
-import {RestaurantMenuComponent} from "./restaurant/child.component/menu/restaurantMenu.component"
-import {RestaurantReviewComponent} from "./restaurant/child.component/review/restaurantReview.component";
+import {RestaurantComponent} from "./Components/restaurant/restaurant.component";
+import {RestaurantMenuComponent} from "./Components/restaurant/child.component/menu/restaurantMenu.component"
+import {RestaurantReviewComponent} from "./Components/restaurant/child.component/review/restaurantReview.component";
 
 //restaurant's the child components
 const restaurantRoutes: Routes = [
