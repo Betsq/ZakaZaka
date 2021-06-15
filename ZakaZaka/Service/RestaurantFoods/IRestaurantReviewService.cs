9ff1253d8ae948a83934da29ee170f9ae7b3a432
Betsq9 <betsq9@gmail.com>
2021-06-16 00:08:29 +0300

    Add restaurant review service

---
+++ ZakaZaka/Service/RestaurantFoods/IRestaurantReviewService.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using ZakaZaka.Models.Restaurants;
+
+namespace ZakaZaka.Service.RestaurantFoods
+{
+    public interface IRestaurantReviewService
+    {
+        public Task<IEnumerable<RestaurantReview>> Get(int restaurantId);
+
+        public Task Add(RestaurantReview restaurantReview);
+        
+        public Task Update(RestaurantReview restaurantReview);
+        
+        public Task Remove(int reviewId);
+    }
+}
---
+++ ZakaZaka/Service/RestaurantFoods/RestaurantReviewService.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ZakaZaka.Context;
+using ZakaZaka.Models.Restaurants;
+
+namespace ZakaZaka.Service.RestaurantFoods
+{
+    public class RestaurantReviewService : IRestaurantReviewService
+    {
+        private readonly ApplicationContext _db;
+        private readonly string _errorText = "Restaurant review is null"; 
+
+        public RestaurantReviewService(ApplicationContext db)
+        {
+            _db = db;
+        }
+        
+        public async Task<IEnumerable<RestaurantReview>> Get(int restaurantId)
+        {
+            var reviews = await _db.RestaurantReviews.Where(item => item.RestaurantId == restaurantId).ToListAsync();
+
+            return reviews;
+        }
+
+        public async Task Add(RestaurantReview restaurantReview)
+        {
+            if (restaurantReview == null)
+                throw new NullReferenceException(_errorText);
+            
+            _db.RestaurantReviews.Add(restaurantReview);
+            await _db.SaveChangesAsync();
+        }
+
+        public async Task Update(RestaurantReview restaurantReview)
+        {
+            if (restaurantReview == null)
+                throw new NullReferenceException(_errorText);
+
+            _db.RestaurantReviews.Update(restaurantReview);
+            await _db.SaveChangesAsync();
+        }
+
+        public async Task Remove(int reviewId)
+        {
+            var review = await _db.RestaurantReviews.FindAsync(reviewId);
+
+            if (review == null)
+                throw new Exception("Review not found");
+
+            _db.Remove(review);
+            await _db.SaveChangesAsync();
+        }
+    }
+}
