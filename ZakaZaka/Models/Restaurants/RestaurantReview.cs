1e196391b92e050b3f6741ed0edc4331e5a4283e
Betsq9 <betsq9@gmail.com>
2021-06-15 23:42:12 +0300

    Add model restaurant review

---
+++ ZakaZaka/Models/Restaurants/RestaurantReview.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ZakaZaka.Models.Restaurants
+{
+    public class RestaurantReview
+    {
+        public int Id { get; set; }
+        public string Review { get; set; }
+        public int Assessment { get; set; }
+        public DateTime Time { get; set; }
+        
+        public int RestaurantId { get; set; }
+        public Restaurant Restaurant { get; set; }
+    }
+}
