5ab2fac4070f0499e729b2d4a67a05697a5206f0
Betsq9 <betsq9@gmail.com>
2021-06-16 22:32:47 +0300

    Add data service for the restaurant review

---
+++ ZakaZaka/ClientApp/src/app/service/Data/restaurantReviewData.service.ts
@@ -0,0 +1,9 @@
+﻿import {Injectable} from "@angular/core";
+import {DataService} from "./Data.service";
+import {HttpClient} from "@angular/common/http";
+
+export class RestaurantReviewDataService extends DataService{
+  constructor(public http: HttpClient) {
+    super(http);
+  }
+}
