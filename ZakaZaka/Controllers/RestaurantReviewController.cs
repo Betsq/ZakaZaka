1c2759b486e848604cddf042a0064fc022a3e8c9
Betsq9 <betsq9@gmail.com>
2021-06-16 22:28:02 +0300

    Add controller for restaurant review

---
+++ ZakaZaka/Controllers/RestaurantReviewController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ZakaZaka.Context;
+using ZakaZaka.Models.Restaurants;
+using ZakaZaka.Service.FormDataBinder;
+using ZakaZaka.Service.RestaurantServices;
+
+namespace ZakaZaka.Controllers
+{
+    [ApiController]
+    [Route("api/review")]
+    public class RestaurantReviewController : Controller
+    {
+        private IRestaurantReviewService _reviewService;
+        
+        public RestaurantReviewController(ApplicationContext db)
+        {
+            _reviewService = new RestaurantReviewService(db);
+        }
+
+        [HttpGet("{restaurantId}")]
+        public async Task<IEnumerable<RestaurantReview>> Get(int restaurantId) => await _reviewService.Get(restaurantId);
+
+
+        [HttpPost]
+        public async Task<IActionResult> Post(
+            [FromForm][ModelBinder(BinderType = typeof(FormDataJsonBinder))]RestaurantReview restaurantReview)
+        {
+            if (restaurantReview == null)
+                return BadRequest();
+
+            restaurantReview.Time = DateTime.Now;
+            
+            await _reviewService.Add(restaurantReview);
+            return Ok();
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Put(
+            [FromForm][ModelBinder(BinderType = typeof(FormDataJsonBinder))]RestaurantReview restaurantReview)
+        {
+            if (restaurantReview == null)
+                return BadRequest();
+            
+            await _reviewService.Update(restaurantReview);
+            return Ok();
+        }
+
+        [HttpDelete("{reviewId}")]
+        public async Task<IActionResult> Delete(int reviewId)
+        {
+            await _reviewService.Remove(reviewId);
+
+            return Ok();
+        }
+    }
+}
