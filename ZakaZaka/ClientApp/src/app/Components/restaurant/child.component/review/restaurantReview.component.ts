73190bf329d1259a642fa24aad351d78f9561855
Betsq9 <betsq9@gmail.com>
2021-06-16 22:36:49 +0300

    Implement restaurant review component

--- ZakaZaka/ClientApp/src/app/Components/restaurant/child.component/review/restaurantReview.component.ts
+++ ZakaZaka/ClientApp/src/app/Components/restaurant/child.component/review/restaurantReview.component.ts
@@ -1,11 +1,41 @@
-﻿import {Component} from "@angular/core";
+﻿import {Component, OnInit} from "@angular/core";
+import {RestaurantDataShareService} from "../../../../service/restaurant/RestaurantDataShare.service";
+import {RestaurantReviewService} from "../../../../service/restaurant/RestaurantReview.service";
+import {RestaurantReview} from "../../../../Model/restaurantReview";
+import {Restaurant} from "../../../../Model/restaurant";
 
 @Component({
   selector: "restaurant-reviews",
   templateUrl: "restaurantReview.component.html",
-  styleUrls: ["restaurantReview.component.less"]
+  styleUrls: ["restaurantReview.component.less"],
+  providers: [RestaurantReviewService]
 })
 
-export class RestaurantReviewComponent{
+export class RestaurantReviewComponent implements OnInit{
+  reviews: RestaurantReview[] = [];
 
+  reviewPost: RestaurantReview = new RestaurantReview();
+  restaurant: Restaurant;
+
+  constructor(private shareData: RestaurantDataShareService, private restaurantReview: RestaurantReviewService)  {
+  }
+
+  ngOnInit() {
+    this.shareData.getComments().subscribe((data: RestaurantReview[]) => this.reviews = data);
+    this.shareData.getRestaurant().subscribe((data: Restaurant) => this.restaurant = data);
+  }
+
+  public add(textareaElem: any){
+    textareaElem.value = "";
+
+    //This is temporary
+    this.reviewPost.assessment = 4;
+    this.restaurantReview.add(this.reviewPost, this.restaurant.id);
+
+    this.loadReviews();
+  }
+
+  public loadReviews(){
+    this.reviews = this.restaurantReview.get(this.restaurant.id);
+  }
 }
