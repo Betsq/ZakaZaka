d04444fcabc5ba165106dd00e4248d3d0e586eff
Betsq9 <betsq9@gmail.com>
2021-06-16 22:32:04 +0300

    Add service for the restaurant review

---
+++ ZakaZaka/ClientApp/src/app/service/restaurant/IRestaurantReview.service.ts
@@ -0,0 +1,8 @@
+﻿import {RestaurantReview} from "../../Model/restaurantReview";
+
+export interface IRestaurantReviewService{
+  get(restaurantId: number) : RestaurantReview[];
+  add(restaurantReview: RestaurantReview, restaurantId: number) : void;
+  update(restaurantReview: RestaurantReview) : void;
+  remove(reviewId: number) : void;
+}
---
+++ ZakaZaka/ClientApp/src/app/service/restaurant/RestaurantReview.service.ts
@@ -0,0 +1,44 @@
+﻿import {Injectable} from "@angular/core";
+import {IRestaurantReviewService} from "./IRestaurantReview.service";
+import { RestaurantReview} from "../../Model/restaurantReview";
+import {RestaurantReviewDataService} from "../Data/restaurantReviewData.service";
+import {GenerateFormService} from "../generateForm/generateForm.service";
+import {HttpClient} from "@angular/common/http";
+
+@Injectable()
+export class RestaurantReviewService implements IRestaurantReviewService{
+  private data : RestaurantReviewDataService;
+
+  constructor(public form: GenerateFormService, private http: HttpClient) {
+    this.data = new RestaurantReviewDataService(http);
+    this.data.url = "api/review"
+  }
+
+  add(restaurantReview: RestaurantReview, restaurantId: number): void {
+    restaurantReview.restaurantId = restaurantId;
+
+    let review = [{name: "restaurantReview", param: restaurantReview}];
+    let form = this.form.generate(review);
+
+    this.data.Post(form).subscribe();
+  }
+
+  get(restaurantId: number): RestaurantReview[] {
+    let reviews: RestaurantReview[];
+
+    this.data.GetWithId(restaurantId).subscribe((data: RestaurantReview[]) => reviews = data);
+    return reviews;
+  }
+
+  remove(reviewId: number): void {
+    this.data.Delete(reviewId).subscribe();
+  }
+
+  update(restaurantReview: RestaurantReview): void {
+    let review = [{name: "restaurantReview", param: restaurantReview}];
+    let form = this.form.generate(review);
+
+    this.data.Put(form).subscribe();
+  }
+
+}
