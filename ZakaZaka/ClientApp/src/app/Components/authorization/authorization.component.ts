e8d303aa65a91792a04638f3ad6757f294059d52
Betsq9 <betsq9@gmail.com>
2021-06-18 20:26:11 +0300

    Add component authorization. This popup component

---
+++ ZakaZaka/ClientApp/src/app/Components/authorization/authorization.component.ts
@@ -0,0 +1,20 @@
+﻿import {Component,EventEmitter, Input, Output} from "@angular/core";
+
+@Component({
+  selector: "authorization",
+  templateUrl: "authorization.component.html",
+  styleUrls: ["authorization.component.less"],
+})
+
+export class AuthorizationComponent{
+  @Input() showSignIn: boolean = false;
+  @Input() showSignUp: boolean = false;
+
+  @Output() closeAuthorization = new EventEmitter();
+  @Output() changeAuthorizationForm = new EventEmitter();
+
+  isShowPassword(element: HTMLInputElement){
+    const type = element.getAttribute("type") === "password" ? "text" : "password";
+    element.setAttribute("type", type);
+  }
+}
