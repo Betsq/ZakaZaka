3dd66d7f19d624915493496756658c9f4296aef2
Betsq9 <betsq9@gmail.com>
2021-06-18 20:27:27 +0300

    implement authorization component in the header component

--- ZakaZaka/ClientApp/src/app/Components/header/header.component.ts
+++ ZakaZaka/ClientApp/src/app/Components/header/header.component.ts
@@ -1,4 +1,4 @@
-﻿import { Component} from "@angular/core";
+﻿import {Component} from "@angular/core";
 
 @Component({
   selector: "main-header",
@@ -6,6 +6,18 @@
   styleUrls: ["header.component.css"],
 })
 
-export class Header{
+export class Header {
+  showSignIn: boolean = false;
+  showSignUp: boolean = false;
 
+  public authorization(showSignIn: boolean, showSignUp: boolean) {
+    this.showSignIn = showSignIn;
+    this.showSignUp = showSignUp;
+  }
+
+  public changeAuthorizationForm(){
+    this.showSignIn = this.showSignIn !== true;
+    this.showSignUp = this.showSignUp !== true;
+
+  }
 }
