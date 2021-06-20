ae2ccebb34403197d3ef2e6c711fd7c21b5191ea
Betsq9 <betsq9@gmail.com>
2021-06-20 22:44:43 +0300

    Add helpers for the jwt

---
+++ ZakaZaka/Helpers/Tokens.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
+using ZakaZaka.Auth;
+using ZakaZaka.Models;
+
+namespace ZakaZaka.Helpers
+{
+    public class Tokens
+    {
+        public static async Task<string> GenerateJwt(ClaimsIdentity identity, IJwtFactory jwtFactory,string userName, JwtIssuerOptions jwtOptions, JsonSerializerSettings serializerSettings)
+        {
+            var response = new
+            {
+                id = identity.Claims.Single(c => c.Type == "id").Value,
+                auth_token = await jwtFactory.GenerateEncodedToken(userName, identity),
+                expires_in = (int)jwtOptions.ValidFor.TotalSeconds
+            };
+
+            return JsonConvert.SerializeObject(response, serializerSettings);
+        }
+
+        public static SymmetricSecurityKey SigningKey()
+        {
+            const string secretKey = "iNivDmHLpUA223sqsfhqGbMRdRj1PVkH";
+            
+            return new SymmetricSecurityKey(Encoding.ASCII.GetBytes(secretKey));
+        }
+    }
+}
