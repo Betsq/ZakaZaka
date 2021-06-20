db030e8b61644bb06544e8334ba0f5c1b66960bf
Betsq9 <betsq9@gmail.com>
2021-06-20 22:42:49 +0300

    Add view models for identity

---
+++ ZakaZaka/ViewModels/CredentialsViewModel.cs
@@ -0,0 +1,10 @@
+﻿using ZakaZaka.ViewModels.Validations;
+
+namespace ZakaZaka.ViewModels
+{
+    public class CredentialsViewModel
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ ZakaZaka/ViewModels/RegistrationViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ZakaZaka.ViewModels
+{
+    public class RegistrationViewModel
+    {
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        [Compare("Password", ErrorMessage = "Password mismatch")]
+        public string ConfirmPassword { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+    }
+}
