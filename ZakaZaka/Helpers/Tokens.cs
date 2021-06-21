7b18d1286fed1af6cfe1858e2f5fbdf557fb6d67
Betsq9 <betsq9@gmail.com>
2021-06-21 23:05:26 +0300

    Add validate method

--- ZakaZaka/Helpers/Tokens.cs
+++ ZakaZaka/Helpers/Tokens.cs
@@ -1,7 +1,10 @@
-﻿using System.Linq;
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
 using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
 using Newtonsoft.Json;
 using ZakaZaka.Auth;
@@ -11,22 +14,52 @@ namespace ZakaZaka.Helpers
 {
     public class Tokens
     {
-        public static async Task<string> GenerateJwt(ClaimsIdentity identity, IJwtFactory jwtFactory,string userName, JwtIssuerOptions jwtOptions, JsonSerializerSettings serializerSettings)
+        public static async Task<string> GenerateJwt(
+            ClaimsIdentity identity,
+            IJwtFactory jwtFactory,
+            string userName,
+            JwtIssuerOptions jwtOptions,
+            JsonSerializerSettings serializerSettings)
         {
             var response = new
             {
                 id = identity.Claims.Single(c => c.Type == "id").Value,
                 auth_token = await jwtFactory.GenerateEncodedToken(userName, identity),
-                expires_in = (int)jwtOptions.ValidFor.TotalSeconds
+                expires_in = (int) jwtOptions.ValidFor.TotalSeconds
             };
 
             return JsonConvert.SerializeObject(response, serializerSettings);
         }
 
+        public static bool Validate(string token, IConfiguration configuration)
+        {
+            var jwtAppSettingOptions = configuration.GetSection(nameof(JwtIssuerOptions));
+            var tokenHandler = new JwtSecurityTokenHandler();
+            try
+            {
+                tokenHandler.ValidateToken(token, new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    ValidateIssuer = true,
+                    ValidIssuer = jwtAppSettingOptions[nameof(JwtIssuerOptions.Issuer)],
+                    ValidateAudience = true,
+                    ValidAudience = jwtAppSettingOptions[nameof(JwtIssuerOptions.Audience)],
+                    ClockSkew = TimeSpan.Zero,
+                    IssuerSigningKey = SigningKey(),
+                }, out SecurityToken validatedToken);
+            }
+            catch
+            {
+                return false;
+            }
+
+            return true;
+        }
+
         public static SymmetricSecurityKey SigningKey()
         {
             const string secretKey = "iNivDmHLpUA223sqsfhqGbMRdRj1PVkH";
-            
+
             return new SymmetricSecurityKey(Encoding.ASCII.GetBytes(secretKey));
         }
     }
