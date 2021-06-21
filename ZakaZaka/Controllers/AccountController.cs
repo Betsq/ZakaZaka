ef353a0317e6cc20e342c0077d4f16c4bcb2bb99
Betsq9 <betsq9@gmail.com>
2021-06-21 23:12:15 +0300

    Add account controller. Implement the post methond for create a user

---
+++ ZakaZaka/Controllers/AccountController.cs
@@ -0,0 +1,45 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ZakaZaka.Context;
+using ZakaZaka.Helpers;
+using ZakaZaka.Models.Identity;
+using ZakaZaka.Service.FormDataBinder;
+using ZakaZaka.ViewModels;
+
+namespace ZakaZaka.Controllers
+{
+    [Route("api/account")]
+    public class AccountController : Controller
+    {
+        private readonly ApplicationContext _db;
+        private readonly UserManager<User> _userManager;
+
+        public AccountController(ApplicationContext db, UserManager<User> userManager)
+        {
+            _db = db;
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody]RegistrationViewModel model)
+        {
+            if (!ModelState.IsValid) return BadRequest(model);
+
+            var user = new User()
+            {
+                Email = model.Email,
+                UserName = model.Email,
+                FirstName = model.FirstName,
+                LastName =  model.LastName
+            };
+
+            var result = await _userManager.CreateAsync(user, model.Password);
+
+            if (!result.Succeeded) return new BadRequestObjectResult(Errors.AddErrorsToModelState(result, ModelState));
+
+            return Ok();
+        }
+    }
+}
