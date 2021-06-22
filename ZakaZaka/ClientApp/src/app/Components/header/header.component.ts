a95a8843156cd9c45d6b153b9d85a145a71f42ed
Betsq9 <betsq9@gmail.com>
2021-06-22 23:07:35 +0300

    Add interaction with auth component

--- ZakaZaka/ClientApp/src/app/Components/header/header.component.ts
+++ ZakaZaka/ClientApp/src/app/Components/header/header.component.ts
@@ -1,14 +1,30 @@
-﻿import {Component} from "@angular/core";
+﻿import {Component, OnInit} from "@angular/core";
+import {UserService} from "../../service/auth/user.service";
+import {Observable} from "rxjs";
+import {TokenService} from "../../service/auth/token.service";
+import {Token} from "@angular/compiler";
 
 @Component({
   selector: "main-header",
   templateUrl: "header.component.html",
   styleUrls: ["header.component.css"],
+  providers: [TokenService]
 })
 
-export class Header {
+export class Header implements OnInit{
+  userName: string;
+  userLogin: boolean = false;
   showSignIn: boolean = false;
   showSignUp: boolean = false;
+
+  constructor(private token: TokenService) {}
+
+   ngOnInit() {
+    this.token.validate().subscribe(value => {
+      this.userLogin = value;
+      this.userName = localStorage.getItem("firstName");
+    });
+   }
 
   public authorization(showSignIn: boolean, showSignUp: boolean) {
     this.showSignIn = showSignIn;
@@ -18,6 +34,16 @@ export class Header {
   public changeAuthorizationForm(){
     this.showSignIn = this.showSignIn !== true;
     this.showSignUp = this.showSignUp !== true;
+  }
 
+  public setIsLogin(isLogin: boolean){
+    this.userName = localStorage.getItem("firstName")
+    this.userLogin = isLogin;
+  }
+
+  logOut(){
+    this.userName = "";
+    this.userLogin = false;
+    this.token.remove();
   }
 }
