52771b04f59f8cdbd210038c0716f21b8216ee0b
Betsq9 <betsq9@gmail.com>
2021-06-22 23:05:40 +0300

    Add service for the user

---
+++ ZakaZaka/ClientApp/src/app/service/auth/user.service.ts
@@ -0,0 +1,27 @@
+﻿import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {Injectable} from "@angular/core";
+import {Observable} from "rxjs";
+import {UserRegistration} from "../../Model/user.registration";
+import {UserLogin} from "../../Model/user.login";
+import {TokenService} from "./token.service";
+
+@Injectable()
+
+export class UserService{
+  private urlLogin: string = "/api/auth/login"
+  private urlRegister: string = "/api/account"
+
+  constructor(private http: HttpClient, private token: TokenService) {}
+
+  register(body: UserRegistration) : Observable<UserRegistration>{
+    let bodyJSON = JSON.stringify(body);
+
+    return this.http.post<UserRegistration>(this.urlRegister, bodyJSON, {headers: new HttpHeaders({"Content-Type": "application/json"})});
+  }
+
+  login(body: UserLogin) : Observable<any>{
+    let bodyJSON = JSON.stringify(body)
+
+    return this.http.post(this.urlLogin, bodyJSON, {headers: new HttpHeaders({"Content-Type": "application/json"})});
+  }
+}
