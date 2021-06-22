dd00c61a0e3de00a6b1bb06ab83fa2e32521f0fd
Betsq9 <betsq9@gmail.com>
2021-06-22 23:05:17 +0300

    Add guard service for the protecting paths

---
+++ ZakaZaka/ClientApp/src/app/service/auth/auth.guard.service.ts
@@ -0,0 +1,13 @@
+﻿import {Injectable} from "@angular/core";
+import {ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot} from "@angular/router";
+import {Observable} from "rxjs";
+import {TokenService} from "./token.service";
+
+@Injectable()
+export class AuthGuardService implements CanActivate{
+  constructor(private token: TokenService) {
+  }
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) : Observable<boolean> | boolean{
+    return this.token.validate();
+  }
+}
