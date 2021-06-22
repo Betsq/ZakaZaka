2dc10e3dcca316bde12feb58ef7a58d7cde12e1f
Betsq9 <betsq9@gmail.com>
2021-06-22 23:04:07 +0300

    Add token service fro validate, get and remove

---
+++ ZakaZaka/ClientApp/src/app/service/auth/token.service.ts
@@ -0,0 +1,28 @@
+﻿import {Injectable} from "@angular/core";
+import {map} from "rxjs/operators";
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+
+@Injectable({
+  providedIn: "root"
+})
+
+export class TokenService{
+  private urlValidate: string = "/api/auth/validateToken"
+
+  constructor(private http: HttpClient) {}
+
+  public validate(): Observable<boolean> {
+    return this.http.get(this.urlValidate + "?token=" + this.get()).pipe(map((data: boolean) => {
+      return data;
+    }));
+  }
+
+  public get(){
+    return localStorage.getItem("auth_token")
+  }
+
+  public remove() : void{
+    localStorage.removeItem("auth_token");
+  }
+}
