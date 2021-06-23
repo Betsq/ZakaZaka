245a3f6024ad93a91baf600974af02f8a1cb1571
Betsq9 <betsq9@gmail.com>
2021-06-23 20:22:11 +0300

    Rework controller. Pass FileOnServer service

--- ZakaZaka/Controllers/RestaurantController.cs
+++ ZakaZaka/Controllers/RestaurantController.cs
@@ -5,11 +5,10 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using ZakaZaka.Context;
 using ZakaZaka.Models;
-using ZakaZaka.Service.AddingFile;
 using ZakaZaka.Service.FormDataBinder;
-using ZakaZaka.Service.RemovingFile;
 using Microsoft.EntityFrameworkCore;
 using ZakaZaka.Models.Restaurants;
+using ZakaZaka.Service.FileOnServer;
 using ZakaZaka.Service.RestaurantCuisines;
 using ZakaZaka.ViewModels;
 
@@ -19,15 +18,15 @@ namespace ZakaZaka.Controllers
     [Route("api/Restaurant")]
     public class RestaurantController : Controller
     {
-        private IWebHostEnvironment _webHostEnvironment;
+        private readonly IFileOnServer _fileOnServer;
         private readonly ApplicationContext _db;
 
-        public RestaurantController(ApplicationContext db, IWebHostEnvironment environment)
+        public RestaurantController(ApplicationContext db, IFileOnServer fileOnServer)
         {
             _db = db;
-            _webHostEnvironment = environment;
+            _fileOnServer = fileOnServer;
         }
-        
+
         [HttpGet]
         public RestaurantManageViewModel Get()
         {
@@ -35,7 +34,7 @@ namespace ZakaZaka.Controllers
                 .Include(cuisine => cuisine.RestaurantCuisines)
                 .Include(item => item.RestaurantFoods)
                 .ToList();
-            
+
             var cuisines = _db.Cuisines.ToList();
 
             var restaurantManage = new RestaurantManageViewModel()
@@ -56,14 +55,14 @@ namespace ZakaZaka.Controllers
             var restaurant = _db.Restaurants
                 .Include(item => item.RestaurantFoods)
                 .Include(item => item.RestaurantCuisines)
-                    .ThenInclude(item => item.Cuisine)
+                .ThenInclude(item => item.Cuisine)
                 .Include(item => item.RestaurantReviews)
                 .FirstOrDefault(item => item.Id == id);
-            
+
 
             if (restaurant == null)
                 return BadRequest();
-                
+
             return restaurant;
         }
 
@@ -71,29 +70,26 @@ namespace ZakaZaka.Controllers
         public IActionResult Post(
             [FromForm] [ModelBinder(BinderType = typeof(FormDataJsonBinder))]
             Restaurant restaurant,
-            [FromForm]
-            IFormFile file,
+            [FromForm] IFormFile file,
             [FromForm] [ModelBinder(BinderType = typeof(FormDataJsonBinder))]
             List<Cuisine> cuisines
         )
         {
             if (restaurant == null)
                 return BadRequest(ModelState);
-            
+
             _db.Restaurants.Add(restaurant);
             /*Save the database so that when transferring restaurant to restaurantCuisine,
              restaurantCuisine can link the table using the restaurant id*/
             _db.SaveChanges();
-            
+
             if (file != null)
             {
                 const string pathToFolder = "/files/restaurants/logo/";
-                
-                var addImage = new AddImageToServer(file, pathToFolder, file.FileName, _webHostEnvironment);
-            
-                restaurant.PathToImage = addImage.Add();
+
+                restaurant.PathToImage = _fileOnServer.Add(pathToFolder, file);
             }
-            
+
             if (cuisines != null)
             {
                 var restaurantCuisineService = new RestaurantCuisineService(restaurant, cuisines);
@@ -101,41 +97,32 @@ namespace ZakaZaka.Controllers
                 var listRestaurantCuisine = restaurantCuisineService.Add();
                 _db.RestaurantCuisines.AddRange(listRestaurantCuisine);
             }
-            
+
             _db.SaveChanges();
-            
+
             return Ok(restaurant);
         }
-        
+
         [HttpPut]
         public IActionResult Put(
             [FromForm] [ModelBinder(BinderType = typeof(FormDataJsonBinder))]
             Restaurant restaurant,
-            [FromForm]
-            IFormFile file,
+            [FromForm] IFormFile file,
             [FromForm] [ModelBinder(BinderType = typeof(FormDataJsonBinder))]
             List<Cuisine> cuisines
-            )
+        )
         {
             if (restaurant == null)
                 return BadRequest(ModelState);
-            
+
             _db.Restaurants.Update(restaurant);
-            
+
             if (file != null)
             {
-                string  pathToFile = restaurant.PathToImage;
                 const string pathToFolder = "/files/restaurants/logo/";
 
-                if (System.IO.File.Exists(_webHostEnvironment.WebRootPath + pathToFile)) 
-                {
-                    var removeFile = new RemoveFileFromServer(pathToFile, _webHostEnvironment);
-                    removeFile.Remove();
-                }
-                
-                var addImage = new AddImageToServer(file, pathToFolder, file.FileName, _webHostEnvironment);
-                  
-                restaurant.PathToImage = addImage.Add();
+                _fileOnServer.Remove(restaurant.PathToImage);
+                restaurant.PathToImage = _fileOnServer.Add(pathToFolder, file);
             }
 
             if (cuisines != null)
@@ -146,18 +133,16 @@ namespace ZakaZaka.Controllers
                 _db.RestaurantCuisines.AddRange(listRestaurantCuisine);
 
                 var restaurantCuisines = _db.RestaurantCuisines.Where(i => i.RestaurantId == restaurant.Id).ToList();
-                
+
                 var unnecessaryRestaurantCuisine = restaurantCuisineService.Remove(restaurantCuisines);
                 _db.RestaurantCuisines.RemoveRange(unnecessaryRestaurantCuisine);
-                
             }
-            
+
             _db.SaveChanges();
             return Ok(restaurant);
         }
 
         [HttpDelete("{id}")]
-
         public IActionResult Delete(int id)
         {
             var restaurant = _db.Restaurants.Find(id);
@@ -165,14 +150,13 @@ namespace ZakaZaka.Controllers
             if (restaurant == null)
                 return BadRequest();
 
-            if (System.IO.File.Exists(_webHostEnvironment.WebRootPath + restaurant.PathToImage))
+            if (_fileOnServer.Exists(restaurant.PathToImage))
             {
                 string pathToFile = restaurant.PathToImage;
-                
-                var removeFile = new RemoveFileFromServer(pathToFile, _webHostEnvironment);
-                removeFile.Remove();
+
+                _fileOnServer.Remove(pathToFile);
             }
-            
+
             _db.Restaurants.Remove(restaurant);
             _db.SaveChanges();
 
