1b5dc7040654bd55e033ade2eeb09e59bf61404a
Betsq9 <betsq9@gmail.com>
2021-06-26 16:26:50 +0300

    Add DTO for the restauratn review model

--- ZakaZaka/Models/ModelsDTO/RestaurantReviewDTO.cs
+++ ZakaZaka/Models/ModelsDTO/RestaurantReviewDTO.cs
@@ -1,1 +1,25 @@
-﻿
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using ZakaZaka.Models.Restaurants;
+
+namespace ZakaZaka.Models.ModelsDTO
+{
+    public class RestaurantReviewDTO
+    {
+        public int Id { get; set; }
+        
+        [Required]
+        [MinLength(3, ErrorMessage = "Review must be more than three characters")]
+        public string Review { get; set; }
+        
+        [Required (ErrorMessage = "No rating specified")]
+        public int Assessment { get; set; }
+        
+        public DateTime Time { get; set; } = DateTime.Now;
+        
+        [Required]
+        public int RestaurantId { get; set; }
+        public Restaurant Restaurant { get; set; }
+    }
+}
