f9edddc4f9069023d40d5cc600cd76ca5f76b31d
Betsq9 <betsq9@gmail.com>
2021-06-27 19:30:45 +0300

    Add DTOs for the restaurant food model

---
+++ ZakaZaka/Models/ModelsDTO/RestaurantFoodDTO.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ZakaZaka.Models.ModelsDTO
+{
+    public class RestaurantFoodDTO
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+        [Required]
+        public string Ingredient { get; set; }
+        public string PathToImage { get; set; }
+        [Required]
+        public int RestaurantId { get; set; }
+    }
+}
