29b53121423f4badb7b72bc3eb654934b8e174cc
Betsq9 <betsq9@gmail.com>
2021-06-27 19:31:58 +0300

    Add mapping helper for the restaurant food model

---
+++ ZakaZaka/Helpers/AutoMapper/MappingFood.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using ZakaZaka.Models.ModelsDTO;
+using ZakaZaka.Models.Restaurants;
+
+namespace ZakaZaka.Helpers.AutoMapper
+{
+    public class MappingFood : Profile
+    {
+        public MappingFood()
+        {
+            CreateMap<RestaurantFood, RestaurantFoodDTO>();
+            CreateMap<RestaurantFoodDTO, RestaurantFood>();
+        }
+    }
+}
