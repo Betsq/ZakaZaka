7d4d346cb364d749eb6d7706c272d0b2c141aaff
Betsq9 <betsq9@gmail.com>
2021-07-03 19:45:57 +0300

    Add modelDTO for the restaurant

---
+++ ZakaZaka/Models/ModelsDTO/RestaurantDTO.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using ZakaZaka.Models.Restaurants;
+
+namespace ZakaZaka.Models.ModelsDTO
+{
+    public class RestaurantDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string PathToImage { get; set; }
+        public int MinimumOrder { get; set; }
+        public int CostDelivery { get; set; }
+        public double TimeToDelivery { get; set; }
+        public bool PayToCard { get; set; }
+        public IEnumerable<RestaurantCuisine> RestaurantCuisines { get; set; }
+        public IEnumerable<RestaurantFood> RestaurantFoods { get; set; } 
+        public IEnumerable<RestaurantReview> RestaurantReviews { get; set; }
+    }
+}
