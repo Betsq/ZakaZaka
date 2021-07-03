a47e5d84f810530f52d552455eb53e1be8afe801
Betsq9 <betsq9@gmail.com>
2021-07-03 19:46:36 +0300

    Add mapper for the restaurant

---
+++ ZakaZaka/Helpers/AutoMapper/MappingRestaurant.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using ZakaZaka.Models.ModelsDTO;
+using ZakaZaka.Models.Restaurants;
+
+namespace ZakaZaka.Helpers.AutoMapper
+{
+    public class MappingRestaurant : Profile
+    {
+        public MappingRestaurant()
+        {
+            CreateMap<Restaurant, RestaurantDTO>();
+            CreateMap<RestaurantDTO, Restaurant>();
+        }
+    }
+}
