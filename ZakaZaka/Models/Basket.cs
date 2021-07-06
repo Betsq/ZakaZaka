bc2ff8f2cb6228226efcd92c538539334f197621
Betsq9 <betsq9@gmail.com>
2021-07-06 22:17:30 +0300

    Add model basket

---
+++ ZakaZaka/Models/Basket.cs
@@ -0,0 +1,9 @@
+﻿namespace ZakaZaka.Models
+{
+    public class Basket
+    {
+        public int RestaurantId { get; set; }
+        public int ProductId { get; set; }
+        public int ProductCount { get; set; }
+    }
+}
