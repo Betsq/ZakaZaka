e7ea625859eeba4cc6544730384b813860c43cf2
Betsq9 <betsq9@gmail.com>
2021-07-06 22:16:55 +0300

    Add basket component

---
+++ ZakaZaka/ClientApp/src/app/Components/basket/basket.component.ts
@@ -0,0 +1,11 @@
+﻿import {Component} from "@angular/core";
+
+@Component({
+  selector: "basket",
+  templateUrl: "basket.component.html",
+  styleUrls: ["basket.component.less"]
+})
+
+export class BasketComponent{
+
+}
