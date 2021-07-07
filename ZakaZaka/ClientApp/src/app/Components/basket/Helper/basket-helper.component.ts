b5ea7ff3c44d2e36d2d06fcea8125d93d6e1d43c
Betsq9 <betsq9@gmail.com>
2021-07-07 23:17:31 +0300

    Add basket helper component

---
+++ ZakaZaka/ClientApp/src/app/Components/basket/Helper/basket-helper.component.ts
@@ -0,0 +1,54 @@
+﻿import {Component, OnInit} from "@angular/core";
+import {BasketService} from "../../../service/Basket.service";
+
+@Component({
+  selector: "basket-helper",
+  templateUrl: "basket-helper.component.html",
+  styleUrls: ["basket-helper.component.less"]
+})
+
+export class BasketHelperComponent implements OnInit{
+  public price : number;
+  public quantity: number;
+
+  public showHelper : boolean = false;
+
+  constructor(private basket: BasketService) {}
+
+  ngOnInit() {
+    this.basket.getFinalPrice().subscribe(value => this.setPrice(value));
+    this.basket.getFinalQuantity().subscribe(value => this.setQuantity(value));
+  }
+
+  private setPrice(price: number){
+    this.price = price;
+
+    this.showBasketHelper();
+  }
+
+  private setQuantity(quantity: number){
+    this.quantity = quantity;
+
+    this.showBasketHelper();
+  }
+
+
+  private showBasketHelper(){
+    let isValid = this.isValid();
+
+    console.log(isValid);
+    if(isValid){
+      this.showHelper = true;
+    }
+    else {
+      this.showHelper = false;
+    }
+  }
+
+  private isValid() : boolean{
+    if(this.price < 1 && this.quantity < 1)
+      return false;
+
+    return true
+  }
+}
