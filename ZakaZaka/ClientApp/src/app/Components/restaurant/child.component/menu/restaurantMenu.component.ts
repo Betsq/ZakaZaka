8daf170c9cf4001431beb84d20ba71cbbf889c1e
Betsq9 <betsq9@gmail.com>
2021-07-07 23:22:49 +0300

    When add a product to the basket, display the final price and quantity

--- ZakaZaka/ClientApp/src/app/Components/restaurant/child.component/menu/restaurantMenu.component.ts
+++ ZakaZaka/ClientApp/src/app/Components/restaurant/child.component/menu/restaurantMenu.component.ts
@@ -1,7 +1,8 @@
 ﻿import {Component, OnInit} from "@angular/core";
-import {ActivatedRoute} from "@angular/router";
 import {RestaurantFood} from "../../../../Model/restaurantFood";
 import {RestaurantDataShareService} from "../../../../service/restaurant/RestaurantDataShare.service";
+import {ProductInBasket} from "../../../../Model/ProductInBasket";
+import {BasketService} from "../../../../service/Basket.service";
 
 @Component({
   selector: "restaurant-menu",
@@ -12,10 +13,16 @@ import {RestaurantDataShareService} from "../../../../service/restaurant/Restaur
 export class RestaurantMenuComponent implements OnInit{
 
   restaurantFoods: RestaurantFood[] = [];
-  constructor(private shareData: RestaurantDataShareService) {
+  constructor(private shareData: RestaurantDataShareService, private basket: BasketService) {
   }
 
   ngOnInit() {
     this.shareData.getFoods().subscribe((data: RestaurantFood[]) => this.restaurantFoods = data);
+  }
+
+  public addToBasket(foodId: number, rstId: number, price: number) {
+    let product: ProductInBasket = {restaurantId: rstId, productId: foodId, productCount: 1, productPrice: price}
+
+    this.basket.addToBasket(product);
   }
 }
